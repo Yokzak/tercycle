37916442312dfb956046b880b861e3ece8736f0c
SILITONTA <bisaae111@gmail.com>
2026-08-12 21:32:05 +0700

    merge

--- tercycle.sql
+++ tercycle.sql
@@ -1,4 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 12, 2026 at 04:27 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tercycle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+
+CREATE TABLE `cache` (
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache_locks`
+--
+
+CREATE TABLE `cache_locks` (
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_keranjang`
+--
+
+CREATE TABLE `detail_keranjang` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `keranjang_id` bigint(20) UNSIGNED NOT NULL,
+  `produk_id` bigint(20) UNSIGNED NOT NULL,
+  `jumlah_produk` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `detail_penukaran`
@@ -17,11 +79,75 @@ CREATE TABLE `detail_penukaran` (
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `detail_pesanan`
+--
 
+CREATE TABLE `detail_pesanan` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `pesanan_id` bigint(20) UNSIGNED NOT NULL,
+  `produk_id` bigint(20) UNSIGNED NOT NULL,
+  `penjual_id` bigint(20) UNSIGNED NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `harga_satuan` int(10) UNSIGNED NOT NULL,
+  `jumlah_produk` int(10) UNSIGNED NOT NULL,
+  `subtotal_poin` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `failed_jobs`
+--
 
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_batches`
+--
+
+CREATE TABLE `job_batches` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `total_jobs` int(11) NOT NULL,
+  `pending_jobs` int(11) NOT NULL,
+  `failed_jobs` int(11) NOT NULL,
+  `failed_job_ids` longtext NOT NULL,
+  `options` mediumtext DEFAULT NULL,
+  `cancelled_at` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `finished_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `kategori_botol`
@@ -46,6 +172,65 @@ INSERT INTO `kategori_botol` (`id`, `nama_kategori`, `poin_satuan`, `created_at`
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `kategori_produk`
+--
+
+CREATE TABLE `kategori_produk` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keranjang`
+--
+
+CREATE TABLE `keranjang` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `siswa_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '0001_01_01_000000_create_users_table', 1),
+(2, '0001_01_01_000001_create_cache_table', 1),
+(3, '0001_01_01_000002_create_jobs_table', 1),
+(4, '2026_08_11_033838_create_siswa_table', 1),
+(5, '2026_08_12_011238_create_kategori_botol_table', 1),
+(6, '2026_08_12_013107_create_penukaran_botol_table', 1),
+(7, '2026_08_12_015136_create_detail_penukaran_table', 1),
+(8, '2026_08_12_025529_create_riwayat_poins_table', 1),
+(9, '2026_08_12_041756_create_kategori_produk_table', 2),
+(10, '2026_08_12_041916_create_produk_table', 2),
+(11, '2026_08_12_061712_create_keranjang_table', 2),
+(12, '2026_08_12_063411_create_detail_keranjang_table', 2),
+(13, '2026_08_12_071608_create_pesanan_table', 2),
+(14, '2026_08_12_072140_create_detail_pesanan_table', 2),
+(15, '2026_08_12_080004_create_pencairan_poin_table', 2);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `password_reset_tokens`
@@ -60,6 +245,29 @@ CREATE TABLE `password_reset_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `pencairan_poin`
+--
+
+CREATE TABLE `pencairan_poin` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `siswa_id` bigint(20) UNSIGNED NOT NULL,
+  `jumlah_poin` int(10) UNSIGNED NOT NULL,
+  `jumlah_uang` bigint(20) UNSIGNED NOT NULL,
+  `metode` enum('cash','e-wallet','bank') NOT NULL,
+  `provider` varchar(255) DEFAULT NULL,
+  `nama_penerima` varchar(255) NOT NULL,
+  `nomor_tujuan` varchar(255) DEFAULT NULL,
+  `status` enum('menunggu','diproses','disetujui','ditolak','selesai') NOT NULL DEFAULT 'menunggu',
+  `catatan` text DEFAULT NULL,
+  `tanggal_pengajuan` datetime NOT NULL,
+  `tanggal_pencairan` datetime DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `penukaran_botol`
 --
 
@@ -69,6 +277,42 @@ CREATE TABLE `penukaran_botol` (
   `admin_id` bigint(20) UNSIGNED NOT NULL,
   `total_poin` int(10) UNSIGNED NOT NULL DEFAULT 0,
   `tanggal` datetime NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `pembeli_id` bigint(20) UNSIGNED NOT NULL,
+  `total_poin` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `status` enum('menunggu','diproses','selesai') NOT NULL DEFAULT 'menunggu',
+  `tanggal` datetime NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `siswa_id` bigint(20) UNSIGNED NOT NULL,
+  `kategori_produk_id` bigint(20) UNSIGNED NOT NULL,
+  `nama_produk` varchar(255) NOT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `harga_poin` int(10) UNSIGNED NOT NULL,
+  `stok` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `gambar` varchar(255) DEFAULT NULL,
+  `status` enum('tersedia','tidak tersedia') NOT NULL DEFAULT 'tersedia',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -109,7 +353,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('ImW3ZcWrOEd9sxBR6aOYSVRq4TNVPeAeVbedxWNE', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoic3pGR0RIaGdNakxQTW15T3dWQnJrWnF0dGNWT2l2S1Y5S2Y2SWdFVSI7czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6Mzc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9zaXN3YS9kYXNoYm9hcmQiO3M6NToicm91dGUiO047fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1786509862);
+('ml31bFcQIn5Iykj5wqzO5SBUm95iDQw9hbmCfdbP', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiUDkxVjJteXRJem15SDByYnAzc0xpV01kVVNlSmJxcmxCTVZzRU13TCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6Mjp7czozOiJ1cmwiO3M6MzQ6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9wcm9kdWsiO3M6NToicm91dGUiO047fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1786544721);
 
 -- --------------------------------------------------------
 
@@ -136,7 +380,11 @@ CREATE TABLE `siswa` (
 
 INSERT INTO `siswa` (`id`, `user_id`, `nama_lengkap`, `nis`, `kode_siswa`, `kelas`, `jurusan`, `saldo_poin`, `created_at`, `updated_at`) VALUES
 (1, 2, 'Budi Santoso', '12345', 'SIS-001', 'XI', 'RPL', 0, '2026-08-11 21:43:38', '2026-08-11 21:43:38'),
-(2, 3, 'Andi Pratama', '12346', 'SIS-002', 'XI', 'RPL', 0, '2026-08-11 21:43:38', '2026-08-11 21:43:38');
+(2, 3, 'Andi Pratama', '12346', 'SIS-002', 'XI', 'RPL', 0, '2026-08-11 21:43:38', '2026-08-11 21:43:38'),
+(3, 4, 'Faisal Alby', 'NIS-1786515495-442', 'SIS-D1KDVV', '-', '-', 0, '2026-08-11 23:18:15', '2026-08-11 23:18:15'),
+(5, 8, 'alby', '20260001', 'SIS-OVT24', 'XII', 'RPL', 0, '2026-08-12 03:43:49', '2026-08-12 04:20:26'),
+(6, 9, 'kevin', '20260004', 'SIS-0006', 'XII', 'TKJ', 0, '2026-08-12 03:46:09', '2026-08-12 03:46:09'),
+(7, 10, 'jaki', '24250002', 'SIS-24250002', 'XII', 'RPL', 0, '2026-08-12 04:45:07', '2026-08-12 04:45:07');
 
 -- --------------------------------------------------------
 
@@ -163,7 +411,11 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Admin', 'admin@gmail.com', NULL, '$2y$12$82.7WDlwWvqShk7K2/Z9Zexqsh29UsPMSl0ul5HidnDUS8GrHI3Tq', 'admin', NULL, '2026-08-11 21:43:37', '2026-08-11 21:43:37'),
 (2, 'Budi', 'budi@gmail.com', NULL, '$2y$12$OSHSmvJkD/mL7QccTbuB9uH8v71UbFRBzaw4T3K.u2tHTwzifqdAe', 'siswa', NULL, '2026-08-11 21:43:38', '2026-08-11 21:43:38'),
-(3, 'Andi', 'andi@gmail.com', NULL, '$2y$12$rAEeY5hKaemztzCE2x/vO.GMeoxfeUXTYF9/9OgV1UIMt8J.O4yNy', 'siswa', NULL, '2026-08-11 21:43:38', '2026-08-11 21:43:38');
+(3, 'Andi', 'andi@gmail.com', NULL, '$2y$12$rAEeY5hKaemztzCE2x/vO.GMeoxfeUXTYF9/9OgV1UIMt8J.O4yNy', 'siswa', NULL, '2026-08-11 21:43:38', '2026-08-11 21:43:38'),
+(4, 'Faisal Alby', 'bisaae111@gmail.com', NULL, '$2y$12$tje0C78ABOAr96tHMjnaxuvZu79zL/oMrYVGeG00XtOILyG/B2D1u', 'siswa', 'ilqFk5gQGA80E8bSXm6FWdD39IJHCYyDSkmyZLjeXmHK77V5mV8OFXv7RWZx', '2026-08-11 23:18:15', '2026-08-11 23:18:15'),
+(8, 'kepin', 'tes2@gmail.com', NULL, '$2y$12$kdRDKqMhzG77EU0oA26h2.mvrO5dVE0JT60u2EXg3Lp0SMA7U8mDe', 'siswa', NULL, '2026-08-12 03:43:49', '2026-08-12 03:43:49'),
+(9, 'kevin', 'tes3@gmail.com', NULL, '$2y$12$wYhqCMXQkC08tGPKegMG.eR6y4uDBBGcfLG15NHUdwXZJQRB5U68m', 'siswa', NULL, '2026-08-12 03:46:09', '2026-08-12 03:46:09'),
+(10, 'jaki', 'tes4@gmail.com', NULL, '$2y$12$CqAyfymhrOgeUxNPEYmtQuC04qcB3JkDOe/z.9bgFhTcg7d976OzC', 'siswa', NULL, '2026-08-12 04:45:07', '2026-08-12 04:45:07');
 
 --
 -- Indexes for dumped tables
@@ -184,12 +436,29 @@ ALTER TABLE `cache_locks`
   ADD KEY `cache_locks_expiration_index` (`expiration`);
 
 --
+-- Indexes for table `detail_keranjang`
+--
+ALTER TABLE `detail_keranjang`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `detail_keranjang_keranjang_id_produk_id_unique` (`keranjang_id`,`produk_id`),
+  ADD KEY `detail_keranjang_produk_id_foreign` (`produk_id`);
+
+--
 -- Indexes for table `detail_penukaran`
 --
 ALTER TABLE `detail_penukaran`
   ADD PRIMARY KEY (`id`),
   ADD KEY `detail_penukaran_penukaran_id_foreign` (`penukaran_id`),
   ADD KEY `detail_penukaran_kategori_botol_id_foreign` (`kategori_botol_id`);
+
+--
+-- Indexes for table `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `detail_pesanan_pesanan_id_foreign` (`pesanan_id`),
+  ADD KEY `detail_pesanan_produk_id_foreign` (`produk_id`),
+  ADD KEY `detail_pesanan_penjual_id_foreign` (`penjual_id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -218,6 +487,20 @@ ALTER TABLE `kategori_botol`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `kategori_produk`
+--
+ALTER TABLE `kategori_produk`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kategori_produk_nama_kategori_unique` (`nama_kategori`);
+
+--
+-- Indexes for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `keranjang_siswa_id_unique` (`siswa_id`);
+
+--
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
@@ -230,12 +513,34 @@ ALTER TABLE `password_reset_tokens`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indexes for table `pencairan_poin`
+--
+ALTER TABLE `pencairan_poin`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pencairan_poin_siswa_id_foreign` (`siswa_id`);
+
+--
 -- Indexes for table `penukaran_botol`
 --
 ALTER TABLE `penukaran_botol`
   ADD PRIMARY KEY (`id`),
   ADD KEY `penukaran_botol_siswa_id_foreign` (`siswa_id`),
   ADD KEY `penukaran_botol_admin_id_foreign` (`admin_id`);
+
+--
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pesanan_pembeli_id_foreign` (`pembeli_id`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `produk_siswa_id_foreign` (`siswa_id`),
+  ADD KEY `produk_kategori_produk_id_foreign` (`kategori_produk_id`);
 
 --
 -- Indexes for table `riwayat_poins`
@@ -273,9 +578,21 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `detail_keranjang`
+--
+ALTER TABLE `detail_keranjang`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `detail_penukaran`
 --
 ALTER TABLE `detail_penukaran`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -297,15 +614,45 @@ ALTER TABLE `kategori_botol`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `kategori_produk`
+--
+ALTER TABLE `kategori_produk`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `pencairan_poin`
+--
+ALTER TABLE `pencairan_poin`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `penukaran_botol`
 --
 ALTER TABLE `penukaran_botol`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -318,17 +665,24 @@ ALTER TABLE `riwayat_poins`
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `detail_keranjang`
+--
+ALTER TABLE `detail_keranjang`
+  ADD CONSTRAINT `detail_keranjang_keranjang_id_foreign` FOREIGN KEY (`keranjang_id`) REFERENCES `keranjang` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detail_keranjang_produk_id_foreign` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`);
 
 --
 -- Constraints for table `detail_penukaran`
@@ -338,11 +692,44 @@ ALTER TABLE `detail_penukaran`
   ADD CONSTRAINT `detail_penukaran_penukaran_id_foreign` FOREIGN KEY (`penukaran_id`) REFERENCES `penukaran_botol` (`id`) ON DELETE CASCADE;
 
 --
+-- Constraints for table `detail_pesanan`
+--
+ALTER TABLE `detail_pesanan`
+  ADD CONSTRAINT `detail_pesanan_penjual_id_foreign` FOREIGN KEY (`penjual_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `detail_pesanan_pesanan_id_foreign` FOREIGN KEY (`pesanan_id`) REFERENCES `pesanan` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `detail_pesanan_produk_id_foreign` FOREIGN KEY (`produk_id`) REFERENCES `produk` (`id`);
+
+--
+-- Constraints for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD CONSTRAINT `keranjang_siswa_id_foreign` FOREIGN KEY (`siswa_id`) REFERENCES `siswa` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `pencairan_poin`
+--
+ALTER TABLE `pencairan_poin`
+  ADD CONSTRAINT `pencairan_poin_siswa_id_foreign` FOREIGN KEY (`siswa_id`) REFERENCES `siswa` (`id`);
+
+--
 -- Constraints for table `penukaran_botol`
 --
 ALTER TABLE `penukaran_botol`
   ADD CONSTRAINT `penukaran_botol_admin_id_foreign` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `penukaran_botol_siswa_id_foreign` FOREIGN KEY (`siswa_id`) REFERENCES `siswa` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_pembeli_id_foreign` FOREIGN KEY (`pembeli_id`) REFERENCES `siswa` (`id`);
+
+--
+-- Constraints for table `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `produk_kategori_produk_id_foreign` FOREIGN KEY (`kategori_produk_id`) REFERENCES `kategori_produk` (`id`),
+  ADD CONSTRAINT `produk_siswa_id_foreign` FOREIGN KEY (`siswa_id`) REFERENCES `siswa` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `riwayat_poins`
