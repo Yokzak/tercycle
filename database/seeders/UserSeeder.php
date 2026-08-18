e328998279556ffd2ce3009a1a9fae27589d5171
Yokzak <zakyhilman230@gmail.com>
2026-08-19 05:35:45 +0700

    trigger deploy

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Admin',
+            'name' => 'Adminn',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('password'),
             'role' => 'admin',
